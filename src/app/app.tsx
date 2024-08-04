4d8abebc4ad10784e36315713bd7fb369d91e5f4
Kciiwa <155839246+Kciiwa@users.noreply.github.com>
2024-08-04 15:10:40 +0300

    feat: add TapBar
    * feat: added tapbar
    
    * fix: colors changed
    
    * fix: added clsx library, BrowserRouter, links replaced with Link from React Router

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,9 @@
-const App = () => <div className='text-9xl font-extrabold text-primary'>DNS</div>
+import { TapBar } from '@/widgets/tap-bar'
 
+const App = () => (
+  <>
+    <div className='text-9xl font-extrabold text-primary'>DNS</div>
+    <TapBar />
+  </>
+)
 export default App
