9752be7d7e9884c2ae9adfe32d527cb6954f5329
Vera Mstislavtseva <mstislavtsevavv@gmail.com>
2024-08-07 17:38:10 +0300

    feat: add RTK
    * feat: add RTK
    
    * fix: rtk
    
    * fix: rtk 2
    
    * fix: rtk 3

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,13 @@
+import { Providers } from './providers'
 import { TapBar } from '@/widgets/tap-bar'
 
 const App = () => (
-  <>
-    <div className='text-9xl font-extrabold text-primary'>DNS</div>
-    <TapBar />
-  </>
+  <Providers>
+    <>
+      <div className='text-9xl font-extrabold text-primary'>DNS</div>
+      <TapBar />
+    </>
+  </Providers>
 )
+
 export default App
