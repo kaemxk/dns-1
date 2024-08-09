6d36e862b03a6691c3d7c62a7ea215d9cd859c85
Vera Mstislavtseva <mstislavtsevavv@gmail.com>
2024-08-09 13:10:34 +0300

    feat: add tile

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ import typography from '@tailwindcss/typography'
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      'pt-sans': ['PT Sans', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+    colors: {
+      brownFont: '#333',
+      orangeFont: '#fc8507',
+      greyFont: '#8c8c8c',
+    },
     extend: {
+      boxShadow: {
+        tile: '0 1px 2px #00000029',
+        btn: '0 0 0 3px #00000008',
+      },
+      gridTemplateRows: {
+        tile: '22px min-content auto 44px',
+      },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
       },
