d9e2497f5ea294ee0b541e5ea4d6ac269b8a3726
Deny Gu <ttopolllo4ek@gmail.com>
2024-08-09 10:37:56 +0300

    fix: app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,3 @@
-const App = () => <div className='text-9xl font-extrabold text-primary'></div>
+const App = () => <div className='text-9xl font-extrabold text-primary'>DNS</div>
 
 export default App
