339549efea88dae8f6ec17a0195ea00fd787234a
Oleg Kaloshin <toni96stark@gmail.com>
2024-08-15 20:37:44 +0300

    fix inline Run(Vi test) error

--- vite.config.ts
+++ vite.config.ts
@@ -12,25 +12,29 @@ const __API_URL__ = JSON.stringify(
 )
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: './src',
-  server: { port: PORT },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@/': path.resolve(__dirname, './src/'),
-      '@/app': path.resolve(__dirname, './src/app'),
-      '@/pages': path.resolve(__dirname, './src/pages'),
-      '@/widgets': path.resolve(__dirname, './src/widgets'),
-      '@/features': path.resolve(__dirname, './src/features'),
-      '@/shared': path.resolve(__dirname, './src/shared'),
+export default defineConfig(({ command }) => {
+  const isTest = process.env.NODE_ENV === 'test'
+
+  return {
+    root: isTest ? '.' : './src',
+    server: { port: PORT },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@/': path.resolve(__dirname, './src/'),
+        '@/app': path.resolve(__dirname, './src/app'),
+        '@/pages': path.resolve(__dirname, './src/pages'),
+        '@/widgets': path.resolve(__dirname, './src/widgets'),
+        '@/features': path.resolve(__dirname, './src/features'),
+        '@/shared': path.resolve(__dirname, './src/shared'),
+      },
     },
-  },
-  define: {
-    __API_URL__,
-  },
-  test: {
-    environment: 'happy-dom',
-    globals: true,
-  },
+    define: {
+      __API_URL__,
+    },
+    test: {
+      environment: 'happy-dom',
+      globals: true,
+    },
+  }
 })
