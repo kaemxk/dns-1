dbdc5b9e05ff229902fcf96bd8dd5f3778c729c5
VikaMaikevich <101277769+VikaMaikevich@users.noreply.github.com>
2024-08-27 18:54:30 +0300

    feat: add-chat-logic

--- src/widgets/chat/ui/chat.tsx
+++ src/widgets/chat/ui/chat.tsx
@@ -1,58 +1,67 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useLayoutEffect, useRef } from 'react'
 import { IoChatboxSharp, IoCloseOutline, IoSend } from 'react-icons/io5'
 import { PiPaperclipHorizontal } from 'react-icons/pi'
 import { RiRobot3Fill } from 'react-icons/ri'
+import clsx from 'clsx'
 
 type ChatMessage = {
-    author: string
-    message: string
-    timestamp: string
-    isImage: boolean
-  }
-  
- type ChatData = {
-    chatHistory: ChatMessage[]
-  }
+  author: string
+  message: string
+  timestamp: string
+  isImage: boolean
+}
 
 export const Chat: React.FC = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(false)
-    const [messages, setMessages] = useState<ChatMessage[]>([])
-  
-    useEffect(() => {
-        const savedMessages = sessionStorage.getItem('chatMessages');
-        if (savedMessages) {
-          setMessages(JSON.parse(savedMessages))
-        } else {
-          fetch('http://localhost:4000/chatHistory')
-            .then(response => {
-              if (!response.ok) {
-                throw new Error('Network response was not ok');
-              }
-              return response.json();
-            })
-            .then((data: ChatData) => {
-              const updatedChatHistory = data.chatHistory.map(message => ({
-                ...message,
-                timestamp: new Date().toLocaleTimeString()  
-              }));
-              setMessages(updatedChatHistory);
-              sessionStorage.setItem('chatMessages', JSON.stringify(updatedChatHistory))
-            })
-            .catch(error => console.error('Error fetching data:', error))
-        }
-      }, [])
-  
-
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [messages, setMessages] = useState<ChatMessage[]>([])
   const [newMessage, setNewMessage] = useState<string>('')
   const [attachedFiles, setAttachedFiles] = useState<File[]>([])
-  const [showTooltip, setShowTooltip] = useState(false)
+  const messagesContainerRef = useRef<HTMLDivElement | null>(null)
 
   const MAX_FILE_SIZE_MB = 50
   const MAX_FILES = 5
   const ALLOWED_FORMATS = ['doc', 'docx', 'xls', 'xlsx', 'txt', 'pdf', 'jpeg', 'jpg', 'png']
 
+  useEffect(() => {
+    const savedMessages = sessionStorage.getItem('chatMessages')
+    if (savedMessages) {
+      setMessages(JSON.parse(savedMessages))
+    } else {
+      fetch('http://localhost:4000/chatHistory')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok')
+          }
+          return response.json()
+        })
+        .then((data: ChatMessage[]) => {
+          const updatedChatHistory = data.map(message => ({
+            ...message,
+            timestamp: new Date().toLocaleTimeString(),
+          }))
+          setMessages(updatedChatHistory)
+          sessionStorage.setItem('chatMessages', JSON.stringify(updatedChatHistory))
+        })
+        .catch(error => console.error('Error fetching data:', error))
+    }
+  }, [])
+
+  useLayoutEffect(() => {
+    const container = messagesContainerRef.current
+    if (container) {
+      container.scrollTop = container.scrollHeight
+    }
+  }, [messages])
+
   const openChat = () => {
-    setIsOpen(!isOpen)
+    if (!isOpen) {
+      setIsLoading(true)
+      setIsOpen(true)
+      setIsLoading(false)
+    } else {
+      setIsOpen(false)
+    }
   }
 
   const handleSendMessage = () => {
@@ -105,7 +114,6 @@ export const Chat: React.FC = () => {
       setAttachedFiles(prevFiles => [...prevFiles, ...validFiles])
     }
   }
-  
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
@@ -116,36 +124,43 @@ export const Chat: React.FC = () => {
   return (
     <>
       <div
-        className={`${
-          !isOpen ? 'translate-y-8 opacity-0' : 'translate-y-0 opacity-100'
-        } fixed bottom-28 right-6 flex hidden w-[360px] flex-col rounded-lg bg-neutral shadow-[0_4px_12px_0_rgba(0,0,0,0.2)] transition-all duration-700 lg:block`}
+        className={clsx(
+          'fixed bottom-28 right-6 flex hidden w-[360px] flex-col rounded-lg bg-neutral shadow-[0_4px_12px_0_rgba(0,0,0,0.2)] transition-all duration-700 lg:block',
+          {
+            'translate-y-8 opacity-0': !isOpen,
+            'translate-y-0 opacity-100': isOpen,
+          },
+        )}
       >
-        <div className='flex h-12 items-center shadow-[0_8px_20px_0_rgba(0,0,0,0.1)]'>
-          <p className='mx-auto block h-6 text-lg text-neutral-content'>Чат DNS</p>
-        </div>
-        <div className='h-[515px] w-full overflow-y-auto px-4 pt-14'>
+        <p className='flex h-12 items-center justify-center text-lg text-neutral-content shadow-[0_8px_20px_0_rgba(0,0,0,0.1)] duration-700'>
+          Чат DNS
+        </p>
+        <div className='h-[515px] w-full overflow-y-auto px-4 pt-14' ref={messagesContainerRef}>
           {messages.map((message, index) =>
             message.author === 'agent' ? (
               <div
                 key={`agent-${index}`}
-                className={`chat chat-start ${
-                  !isOpen ? 'opacity-0' : 'opacity-100'
-                } duration-700`}
+                className={clsx('chat chat-start duration-700', {
+                  'opacity-0': !isOpen,
+                  'opacity-100': isOpen,
+                })}
               >
                 <div className='avatar chat-image'>
                   <div className='!flex w-10 justify-center rounded-full border'>
                     <RiRobot3Fill className='h-7 w-7 self-center' />
                   </div>
                 </div>
+
                 <div className='chat-bubble chat-bubble-primary'>{message.message}</div>
-                <time className='mt-1 text-xs text-white !important'>{message.timestamp}</time>
+                <time className='mt-1 text-xs opacity-50'>{message.timestamp}</time>
               </div>
             ) : (
               <div
                 key={`user-${index}`}
-                className={`chat chat-end ${
-                  !isOpen ? 'opacity-0' : 'opacity-100'
-                } duration-700`}
+                className={clsx('chat chat-end duration-700', {
+                  'opacity-0': !isOpen,
+                  'opacity-100': isOpen,
+                })}
               >
                 {message.isImage ? (
                   <img
@@ -157,19 +172,14 @@ export const Chat: React.FC = () => {
                   <div className='chat-bubble chat-bubble-accent'>{message.message}</div>
                 )}
 
-                <time className='mt-1 text-xs opacity-50'>{message.timestamp}</time>
+                <time className='mt-1 text-xs text-gray-500 opacity-70'>{message.timestamp}</time>
               </div>
             ),
           )}
         </div>
-        <div className='relative mt-auto flex h-16 items-center justify-start shadow-[8px_0_20px_0_rgba(0,0,0,0.1)]'>
-          <label
-            htmlFor='file-upload'
-            className='relative cursor-pointer'
-            onMouseEnter={() => setShowTooltip(true)}
-            onMouseLeave={() => setShowTooltip(false)}
-          >
-            <PiPaperclipHorizontal className='mx-2 h-7 w-7 rotate-90 text-gray-500' /> {/* Знак стал светлее */}
+        <div className='mt-auto flex h-16 items-center justify-start shadow-[8px_0_20px_0_rgba(0,0,0,0.1)]'>
+          <label htmlFor='file-upload' className='cursor-pointer'>
+            <PiPaperclipHorizontal className='mx-2 h-7 w-7 rotate-90 text-gray-500' />
           </label>
           <input
             id='file-upload'
@@ -178,13 +188,6 @@ export const Chat: React.FC = () => {
             multiple
             onChange={handleAttachImage}
           />
-          {showTooltip && (
-            <div className='absolute bottom-14 left-0 w-[280px] rounded-lg bg-gray-800 p-2 text-xs text-white shadow-lg'>
-              Вы можете прикрепить изображение или документ формата: doc, docx, xls, xlsx, txt, pdf,
-              jpeg, jpg, png. Общий объем файлов не должен превышать <strong>50MB</strong>.
-              Количество документов не более <strong>5</strong>.
-            </div>
-          )}
           <input
             type='text'
             value={newMessage}
@@ -207,7 +210,9 @@ export const Chat: React.FC = () => {
         className='fixed bottom-6 right-6 hidden h-14 w-14 rounded-full border-2 border-primary bg-primary shadow-[0_4px_12px_0_rgba(0,0,0,0.2)] lg:block'
         onClick={openChat}
       >
-        {isOpen ? (
+        {isLoading ? (
+          <span className='loading loading-spinner text-neutral'></span>
+        ) : isOpen ? (
           <IoCloseOutline className='m-auto h-7 w-7 text-primary-content' />
         ) : (
           <IoChatboxSharp className='m-auto h-5 w-5 text-primary-content' />
@@ -216,4 +221,3 @@ export const Chat: React.FC = () => {
     </>
   )
 }
-
