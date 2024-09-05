b5ae177e8c06d36f81e2d863d19895494f7f28c3
VikaMaikevich <101277769+VikaMaikevich@users.noreply.github.com>
2024-09-05 17:37:32 +0300

    feat: add chat logic
    * feat: testing version
    
    * feat: add-chat-logic
    
    * refactor: added utilities, interfaces, and integrated RTK
    
    * ref: added utilities, interfaces, and integrated RTK
    
    * OS5-17-chat-logic
    
    * refactor: migrate chat logic to use RTK Query
    
    * ref: resolved conflicts in JSON and API
    
    ---------
    
    Co-authored-by: Deny <126056658+Deny-Gu@users.noreply.github.com>

---
+++ src/widgets/chat/chatSlice.tsx
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { ChatMessage } from './interfaces'
+
+interface ChatState {
+  messages: ChatMessage[]
+}
+
+const initialState: ChatState = {
+  messages: [],
+}
+
+const chatSlice = createSlice({
+  name: 'chat',
+  initialState,
+  reducers: {
+    addMessage: (state, action: PayloadAction<ChatMessage>) => {
+      state.messages.push(action.payload)
+      sessionStorage.setItem('chatMessages', JSON.stringify(state.messages))
+    },
+    addMessages: (state, action: PayloadAction<ChatMessage[]>) => {
+      state.messages = action.payload
+      sessionStorage.setItem('chatMessages', JSON.stringify(state.messages))
+    },
+  },
+})
+
+export const { addMessage, addMessages } = chatSlice.actions
+export default chatSlice.reducer
