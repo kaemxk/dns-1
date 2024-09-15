f429c5b19529f8fd13c431c0722f626d097ca6ef
Anna <predator190402@gmail.com>
2024-09-15 11:40:16 +0300

    test: brands

---
+++ src/widgets/brands/ui/brands.test.tsx
@@ -0,0 +1,49 @@
+import {fireEvent, render} from '@testing-library/react'
+import {describe, expect, it} from 'vitest'
+import {Brands} from './brands'
+import {MemoryRouter} from 'react-router-dom'
+import '@testing-library/jest-dom/vitest';
+
+type SwiperSlideElem = Element & {
+    swiperSlideIndex: number
+}
+
+describe('Brands', () => {
+    it('Отображение элементов на десктопе', () => {
+        const {container} = render(
+            <MemoryRouter>
+                <Brands/>
+            </MemoryRouter>,
+        )
+
+        fireEvent.resize(window, {target: {innerWidth: 1024}})
+
+        const elements = container.querySelectorAll('.swiper-slide')
+        expect(elements.length).toBe(16)
+
+        elements.forEach((el, idx) => {
+            expect(idx).toBe((el as SwiperSlideElem).swiperSlideIndex)
+            expect(el.getAttribute("style")).toBe("width: 132px;")
+            expect(el.children.item(0)?.getAttribute("style")).toBe("height: 69px;")
+        })
+    })
+    it('Отображение элементов на мобильном устройстве', () => {
+            const {container} = render(
+                <MemoryRouter>
+                    <Brands/>
+                </MemoryRouter>,
+            )
+
+            fireEvent.resize(window, {target: {innerWidth: 500}})
+
+            const elements = container.querySelectorAll('.swiper-slide')
+            expect(elements.length).toBe(16)
+
+            elements.forEach((el, idx) => {
+                expect(idx).toBe((el as SwiperSlideElem).swiperSlideIndex)
+                expect(el.getAttribute("style")).toBe("width: 112px;")
+                expect(el.children.item(0)?.getAttribute("style")).toBe("height: 57px;")
+            })
+        },
+    );
+})
