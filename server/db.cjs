13d733ee2e91e5e7085015da721b2dfac8f52485
Eduard Gotsutsov <166016806+SyntaxSamuraiZero@users.noreply.github.com>
2024-09-23 07:17:00 +0300

    feat: split db file

---
+++ server/db.cjs
@@ -0,0 +1,13 @@
+const catalogMenu = require('./catalogMenu.json')
+const chatHistory = require('./chatHistory.json')
+const footerInfo = require('./footerInfo.json')
+const headerInfo = require('./headerInfo.json')
+const sliderProducts = require('./sliderProducts.json')
+
+module.exports = () => ({
+  headerInfo,
+  footerInfo,
+  catalogMenu,
+  chatHistory,
+  sliderProducts,
+})
