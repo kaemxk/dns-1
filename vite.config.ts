90e8d3af24f578c0404e840b16a41084ceb2121c
KooWeso <106434278+KooWeso@users.noreply.github.com>
2024-10-01 16:11:32 +0300

    revert: dug too deep, oops
    Refs: 5490a08

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,12 @@ const isProduction = NODE_ENV === 'production'
 const __API_URL__ = JSON.stringify(
   isProduction ? 'https://www.production-url.com' : 'http://localhost:4000/',
 )
+const isTest = NODE_ENV === 'test'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: isTest ? '.' : './src',
+  publicDir: isTest ? 'public' : '../public',
   server: { port: PORT },
   plugins: [react()],
   resolve: {
