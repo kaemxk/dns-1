bf3316b578cef1aa515386bda4a7a36bcb92b515
Den1sRRR <denisonka2002@mail.ru>
2024-10-08 19:44:00 +0500

    fix OS5-50-fix-footer-images

--- src/widgets/footer/ui/footerLoadIcon.tsx
+++ src/widgets/footer/ui/footerLoadIcon.tsx
@@ -7,28 +7,28 @@ const FooterLoadIcon = () => {
             <div
               data-testid='footer-load-icon-item'
               className='transition-filter hover:brightness-115 h-10 w-[135px] bg-contain bg-center bg-no-repeat duration-300'
-              style={{ backgroundImage: "url('../widgets/footer/images/googlePlay.svg')" }}
+              style={{ backgroundImage: "url('../widgets/footer/ui/images/googlePlay.svg')" }}
             ></div>
           </a>
           <a href='#'>
             <div
               data-testid='footer-load-icon-item'
               className='transition-filter hover:brightness-115 h-10 w-[135px] bg-contain bg-center bg-no-repeat duration-300'
-              style={{ backgroundImage: "url('../widgets/footer/images/AppGallery.svg')" }}
+              style={{ backgroundImage: "url('../widgets/footer/ui/images/AppGallery.svg')" }}
             ></div>
           </a>
           <a href='#'>
             <div
               data-testid='footer-load-icon-item'
               className='transition-filter hover:brightness-115 h-10 w-[135px] bg-contain bg-center bg-no-repeat duration-300'
-              style={{ backgroundImage: "url('../widgets/footer/images/ruStore.svg')" }}
+              style={{ backgroundImage: "url('../widgets/footer/ui/images/ruStore.svg')" }}
             ></div>
           </a>
           <a href='#'>
             <div
               data-testid='footer-load-icon-item'
               className='transition-filter hover:brightness-115 h-10 w-[135px] bg-contain bg-center bg-no-repeat duration-300'
-              style={{ backgroundImage: "url('../widgets/footer/images/appStore.svg')" }}
+              style={{ backgroundImage: "url('../widgets/footer/ui/images/appStore.svg')" }}
             ></div>
           </a>
         </div>
