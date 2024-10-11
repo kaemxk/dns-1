a69f8f9389f81fbedf5cac6870423511d4f8b9d6
KooWeso <106434278+KooWeso@users.noreply.github.com>
2024-10-11 18:54:05 +0300

    refactor: interface

--- src/shared/ui/side-block/side-block.tsx
+++ src/shared/ui/side-block/side-block.tsx
@@ -2,23 +2,17 @@ import { IconContext } from 'react-icons'
 import { MdOutlineArrowOutward } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
+import { IActiveIconLink, ILink } from './interface'
+
 interface SideBlockProps {
-  top: {
-    title: string
-    href: string
-    active?: boolean
-    icon?: JSX.Element
-  }[]
-  bottom: {
-    title: string
-    href: string
-  }[]
+  top: IActiveIconLink[]
+  bottom?: ILink[]
 }
 
 const SideBlock: React.FC<SideBlockProps> = ({ top, bottom }: SideBlockProps) => {
   return (
     <div className='w-[280px] overflow-hidden rounded-lg bg-white shadow-[0_1px_2px_rgba(0,0,0,.16)]'>
-      <div className='flex w-full flex-col border-b-[1px] border-[#eee] pb-3 pt-3'>
+      <div className='flex w-full flex-col pb-3 pt-3'>
         {top.map(({ title, href, active, icon }) => (
           <Link
             key={title}
@@ -37,18 +31,20 @@ const SideBlock: React.FC<SideBlockProps> = ({ top, bottom }: SideBlockProps) =>
           </Link>
         ))}
       </div>
-      <div className='flex w-full flex-col pb-3 pt-3'>
-        {bottom.map(({ title, href }) => (
-          <Link key={title} className='relative inline-block px-4 py-3 text-[#0d61af]' to={href}>
-            {title}
-            <IconContext.Provider
-              value={{ className: 'text-[#d9d9d9] right-4 inline absolute top-4' }}
-            >
-              <MdOutlineArrowOutward />
-            </IconContext.Provider>
-          </Link>
-        ))}
-      </div>
+      {bottom && (
+        <div className='flex w-full flex-col border-t-[1px] border-[#eee] pb-3 pt-3'>
+          {bottom.map(({ title, href }) => (
+            <Link key={title} className='relative inline-block px-4 py-3 text-[#0d61af]' to={href}>
+              {title}
+              <IconContext.Provider
+                value={{ className: 'text-[#d9d9d9] right-4 inline absolute top-4' }}
+              >
+                <MdOutlineArrowOutward />
+              </IconContext.Provider>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
