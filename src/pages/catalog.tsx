e2af7bde791bc4b22a41255ddca791852e864553
Nikita Bautin <alliancesuck13@yandex.ru>
2024-10-15 21:48:03 +0700

    feat: smooth hover onMouseEnter

--- src/pages/catalog.tsx
+++ src/pages/catalog.tsx
@@ -1,12 +1,10 @@
 import { FC } from 'react'
 
-import Breadcrumbs from './catalog/components/breadcrumbs-TEMP/breadcrumbs'
 import ItemsGrid from './catalog/components/items-grid/items-grid'
 
 const Catolog: FC = () => {
   return (
     <div>
-      <Breadcrumbs />
       <ItemsGrid />
       <div></div>
     </div>
